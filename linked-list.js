1503b0d14ab217f099e58765dcd97b10ce30750a
lentan <lentan1029@gmail.com>
2015-12-04 22:32:00 +0100

    created test suite

---
+++ linked-list.js
@@ -0,0 +1,23 @@
+/*
+Linked list
+Nodes
+
+
+
+
+*/
+var LinkedList = function() {
+
+  return 
+}
+
+var Node = function(a) {
+  this.next = a ? a : null;
+  return this;
+}
+
+
+//tests
+var Test = require("./test.js");
+
+Test.assert("a","a")
--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-console.log("test")
+(function (exports){
+  //exports.assert
+  exports.assert = function(a, b, msg){
+    if(a==b){
+      console.log(msg ? msg : "passed");
+      return 1;
+    } else {
+      console.log("failed");
+      return 0;
+    }
+  };
+
+})(this); //((typeof window === 'undefined') ? module.exports : window));
+
