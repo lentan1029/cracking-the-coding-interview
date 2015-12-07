fbd2e7a934d26f0f64e6b5ae01b119f8809d4305
lentan <lentan1029@gmail.com>
2015-12-07 04:36:18 +0100

    Trie

--- trie.js
+++ trie.js
@@ -55,9 +55,58 @@ Trie.prototype.find = function(word){
 }
 
 Trie.prototype.delete = function(word){
-  //recursively search through word, until we hit tail
-  //backtrack: if parent has only one child, delete
+  if(word == undefined || typeof(word) !== "string" || word.length == 0){ throw new Error("word must be valid"); }
 
+  //iteratively search through chars in word, until we hit tail
+  //backtrack: if parent has only one child, delete (eg.)
+  
+  var curr = this.head;
+
+  function run(curr, word, i){ 
+    //terminates when we reach the end of the word, and returns true;
+    //returns true when we delete a character
+
+    if(i == word.length) {
+      if(curr["tail"]){
+        delete curr["tail"];
+        var toDelete = true;
+        for(key in curr){
+          if(key !== word[i]){
+            toDelete = false
+          }
+        }
+        return toDelete;
+      } else {
+        throw new Error("word not found");
+      }
+    } else {
+      if(curr[word[i]]){ //if word seems to be in the trie: continue
+        if(run(curr[word[i]], word, i+1)){ //if the rest of the word is in the trie and has been deleted
+          var toDelete = true;
+          for(key in curr){
+            if(key !== word[i]){
+              toDelete = false
+            }
+          }
+
+          if(toDelete){
+            delete curr[word[i]];
+            return true;
+          } else {
+            return false;
+          }
+        } else {
+          return false;
+        }
+      } else {
+        throw new Error("word not found")
+      }
+    }
+
+  }
+
+  run(curr, word, 0);
+  return this;
 
 }
 
@@ -73,5 +122,6 @@ Test.assert(t.find("aa"), "word not found");
 Test.assert(t.find("aaaa"), "word found");
 Test.assert(t.find("aaaaf"), "word found");
 Test.assert(t["head"]["a"]["a"]["a"]["a"]["tail"], 1);
-Test.assert(t["head"]["a"]["a"]["a"]["a"]["f"]["tail"], 1)
+Test.assert(t["head"]["a"]["a"]["a"]["a"]["f"]["tail"], 1);
+Test.assert(t.delete("aaaa")["head"]["a"]["a"]["a"]["a"]["f"]["tail"], 1);
 
