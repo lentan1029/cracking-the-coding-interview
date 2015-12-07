923d549f0466701a3dc21acc60b304afd0aaf586
lentan <lentan1029@gmail.com>
2015-12-07 06:24:35 +0100

    Stack

--- stack.js
+++ stack.js
@@ -9,9 +9,42 @@ clear
 
 var LinkedList = require("./linked-list.js");
 
-var L = new LinkedList.LinkedList;
+var Stack = function(){
+  this.l = new LinkedList.LinkedList();
+  this.length = this.l.length;
+}
 
-L.add("asdf");
-console.log(L);
-L.remove("asdf");
-console.log(L);
+Stack.prototype.push = function(data){
+  this.l.add(data);
+  this.length = this.l.length;
+}
+
+Stack.prototype.pop = function(){
+  this.l.remove(this.l.start.data);
+  this.length = this.l.length;
+}
+
+Stack.prototype.isEmpty = function(){
+  return this.length === 0;
+}
+
+Stack.prototype.peek = function(){
+  if(!this.isEmpty()){
+    return this.l.start;
+  }
+}
+
+Stack.prototype.clear = function(){
+  this.l = new LinkedList.LinkedList();
+  this.length = this.l.length;
+}
+
+//tests
+var Test = require("./test.js");
+
+var s = new Stack();
+s.push("thing");
+Test.assert(s.length, 1);
+Test.assert(s.peek().data, "thing");
+s.pop();
+Test.assert(s.length, 0);
