7b5b5c40b010988eb3f327f248221f9a6814a395
lentan <lentan1029@gmail.com>
2015-12-07 08:03:59 +0100

    Heap

--- heap.js
+++ heap.js
@@ -3,7 +3,7 @@ Heap (array representation)
 Insert (bubble up)
 Delete (bubble down)
 
-Get parent: ~~(n / 2)
+Get parent: ~~(n / 2) 
 Get sibling: (n % 2) ? n - 1 : n + 1
 Get children: n * 2 || n * 2 + 1
 */
@@ -24,7 +24,6 @@ var Heap = function(cmp){
 Heap.prototype.insert = function(item){
   if(item == null){ throw new Error("data must be valid"); }
   this.data.push(item);
-
 
   // bubble up
   var n = this.data.length;
@@ -59,6 +58,53 @@ Heap.prototype.isSatisfied = function(){
   return true;
 }
 
+Heap.prototype.remove = function(item){
+
+  function i(n){
+    return n-1;
+  }
+
+  var n = this.data.indexOf(item) + 1;
+
+  if(n){
+    this.data.splice(n - 1, 1, this.data[this.data.length-1]);
+    this.data.pop();
+
+    //bubble down
+    while(n * 2 < this.data.length){ //terminate when we have no more children
+      var leftChild = this.data[i(n*2)];
+      var rightChild = this.data[i(n*2+1)];
+
+      //swap with the biggest
+
+      if(this.cmp(leftChild, rightChild) || rightChild == null){ //if leftChild > rightChild
+        if(!this.cmp(this.data[i(n)], this.data[i(n*2)])){ //if ordering not satsified
+          var tmp = this.data[i(n)];
+          this.data[i(n)] = this.data[i(n*2)];
+          this.data[i(n*2)] = tmp;
+          n = n * 2;
+        } else {
+          break; //ordering satisfied, exit
+        }
+      } else {
+        if(!this.cmp(this.data[i(n)], this.data[i(n*2+1)])){
+          var tmp = this.data[i(n)];
+          this.data[i(n)] = this.data[i(n*2+1)];
+          this.data[i(n*2+1)] = tmp;
+          n = n*2+1;
+        } else {
+          break;
+        }
+      }
+
+    }
+
+  }
+
+  return this;
+
+}
+
 
 //tests
 var Test = require("./test.js");
@@ -67,4 +113,5 @@ var H = new Heap((a,b) => a > b);
 
 [1,2,3,4,7,8,9,10,14,16,17].map((item)=>H.insert(item));
 
+Test.assert(H.isSatisfied(), true);
 Test.assert(H.isSatisfied(), true);
